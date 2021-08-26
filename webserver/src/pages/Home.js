2aa53b0ce3f83981483a8fbe7d6b210eeadfcd8e
Magdeline Huang <54191678+xsol05@users.noreply.github.com>
2021-08-27 00:00:04 +1200

    Make all pages accessible from Home

--- webserver/src/pages/Home.js
+++ webserver/src/pages/Home.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Home() {
     return (
-        <h1>Home!</h1>
+        <>
+            <h1>Dashboard</h1>
+            <Link to="/login">Log in</Link>
+            <Link to="/registerselect">Register (select user)</Link>
+            <Link to="/registermanager">Register Manager</Link>
+            <Link to="/registermember">Register Member</Link>
+            <Link to="/dashboard">Dashboard</Link>
+            <Link to="/serviceform">Service Form</Link>
+        < />
     );
 }
 
