d25b30950d97b2ea1b6329c12d5747d5ef34c627
Ethan Fraser <ejpfraser@gmail.com>
2021-08-29 10:56:46 +1200

    Updated db setup script (unfinished)

--- dbserver/setup-database.sql
+++ dbserver/setup-database.sql
@@ -1,12 +1,52 @@
-drop table if exists users;
+drop table payments;
+drop table users;
+drop table services;
+drop table flats;
 
-create table users (
-    email   varchar(128) not null,
-    fname   varchar(64),
-    lname   varchar(64),
-    passwd  varchar(60),
-    primary key(email)
+
+create table flats (
+    flatID      varchar(10) primary key,
+    name        varchar(256)
 );
 
-insert into users values ("ejpfraser@gmail.com", "Ethan", "Fraser", "$2b$09$JUdvy5Jl2vqX8f/cNKMVgOX0o0dRvk9DEGRGhiTUpXgOZgF9sAboO");
-insert into users values ("magdeline0512@gmail.com", "Magdeline", "Huang", "$2b$09$pEP/BapTPw48cElxAkH4eeIHSAJOc3vC7kuQHOt.fBWXOgs2vTiOq");
+create table users (
+    email       varchar(128) primary key,
+    fname       varchar(64),
+    lname       varchar(64),
+    passwd      varchar(60),
+    is_manager  boolean default 0,
+    flatID      varchar(10) not null,
+    constraint fk_users_flatID
+    foreign key (flatID)
+        references flats(flatID)
+);
+
+create table services (
+    name        varchar(256) primary key,
+    period      varchar(11),
+    day         varchar(9),
+    due         date,
+    is_static   boolean default 0,
+    amount      decimal(13,2) default 0.00,
+    flatID      varchar(10) not null,
+    constraint  check_billingCycle
+    check (period in ('daily', 'weekly', 'fortnightly')
+        and day in ('monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday')),
+    constraint  fk_services_flatID
+    foreign key (flatID)
+        references flats(flatID)
+);
+
+create table payments (
+    paymentID   int primary key auto_increment,
+    amount      decimal(13,2) default 0.00,
+    verified    boolean default 0,
+    user_from   varchar(128) not null,
+    service_for varchar(256) not null,
+    constraint fk_userFrom
+    foreign key (user_from)
+        references users(email),
+    constraint fk_serviceFor
+    foreign key (service_for)
+        references services(name)
+);
