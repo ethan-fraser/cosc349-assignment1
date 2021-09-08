5d39cc088919c56b6f2bcad493898d4d26e7ab47
Magdeline Huang <54191678+xsol05@users.noreply.github.com>
2021-09-08 13:37:12 +1200

    Add favicon and logo to navbar

--- webserver/src/pages/Dashboard.js
+++ webserver/src/pages/Dashboard.js
@@ -270,7 +270,10 @@ class Dashboard extends React.Component {
         return (
             <div>
                 <nav className="bg-blue-400 flex flex-row">
-                    <h1 className="text-3xl text-gray-50 font-black text-left py-3 pl-5"><Link to="/dashboard">flatbills</Link></h1>
+                    <h1 className="text-3xl text-gray-50 font-black text-left py-3 px-3 inline-flex">
+                        <img src='/flatbills_logo.png' alt="flatbills logo" width="35" height="35" className="mr-2"></img>
+                        <Link to="/dashboard">flatbills</Link>
+                    </h1>
                     <button
                         className="font-semibold text-blue-400 bg-white hover:bg-gray-50 rounded w-36 py-2 px-2 my-3 mr-5 absolute right-0"
                         onClick={ () => this.doLogout() }
--- webserver/src/pages/ServiceForm.js
+++ webserver/src/pages/ServiceForm.js
@@ -91,7 +91,10 @@ class ServiceForm extends React.Component {
         return (
             <div>
                 <nav className="bg-blue-400 flex flex-row">
-                    <h1 className="text-3xl text-gray-50 font-black text-left py-3 pl-5"><Link to="/dashboard">flatbills</Link></h1>
+                    <h1 className="text-3xl text-gray-50 font-black text-left py-3 px-3 inline-flex">
+                        <img src='/flatbills_logo.png' alt="flatbills logo" width="35" height="35" className="mr-2"></img>
+                        <Link to="/dashboard">flatbills</Link>
+                    </h1>
                     <button className="font-semibold text-blue-400 bg-white hover:bg-gray-50 rounded w-36 py-2 px-2 my-3 mr-5 absolute right-0"><Link to="/login">Logout</Link></button>                
                 </nav>
                 <div className="flex flex-row pt-10 items-center">
